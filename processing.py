0aaeab0626dfc7c571858e257e9603de07e8177d
koline2024 <christopherxia2020@gmail.com>
2025-11-12 00:38:16 -0600

    Plan part 1 sections c and d complete: c. Plot the waveform of the .wav WRT time. (Done) d. Compute and plot the magnitude spectrum FFT of the .wav. (Done)

--- processing.py
+++ processing.py
@@ -1,6 +1,10 @@
-from pathlib import path
 import numpy as np
-import scipy.signal as sp
+from scipy.fft import fft
 import matplotlib.pyplot as plt
 
-class processing:
+xSignalField = np.linspace(0, 4*np.pi, num = 256)
+ySignal = np.sin(xSignalField) + 2*np.sin(xSignalField * 4) + 5*np.sin(xSignalField * 10)
+yAmp = fft(ySignal)
+
+plt.plot(xSignalField, yAmp)
+plt.show()
